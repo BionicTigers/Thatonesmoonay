bc2c59cb619e1bddedfe811222244e3d651589e4
Nathan Stewart <natedawg1121@gmail.com>
2018-10-09 10:29:32 -0400

    Small changes to 4 motor TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp4motors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp4motors.java
@@ -3,104 +3,102 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 
 @TeleOp(name="4Motors", group="BTBT")
 public class TeleOp4motors extends OpMode {
 
-        //Drivetrain Motors//
-        private DcMotor backLeft; //left motor back
-        private DcMotor frontLeft; //left motor front (Ladies first)ot
-        private DcMotor backRight; //right motor back
-        private DcMotor frontRight; //right motor front (Ladies first)
-        private DcMotor collector;
-        private DcMotor evangelino;
-        private Servo teamMarker;
+    //Drivetrain Motors//
+    private DcMotor backLeft; //left motor back
+    private DcMotor frontLeft; //left motor front (Ladies first)ot
+    private DcMotor backRight; //right motor back
+    private DcMotor frontRight; //right motor front (Ladies first)
+//    private DcMotor collector;
+//    private DcMotor evangelino;
+//    private Servo teamMarker;
+
+    //Variables//
+    private double xValue, yValue, leftPower, rightPower, coarseDiff, fineDiff, stickDiff, xOffset;
+    public double calibToggle;
+    public int driveMode;
+//    public int target;
+//    private double speed;
+//    private Servo liftrawrh;
+
+    //Objects//
+    public ElapsedTime runtime = new ElapsedTime();
+
+    //HardwareCatBot robot;
+
+    public void init() {
+        //Motors//
+        backLeft = hardwareMap.dcMotor.get("backLeft"); //Left Back
+        frontLeft = hardwareMap.dcMotor.get("frontLeft"); //Left Front
+        backRight = hardwareMap.dcMotor.get("backRight"); //Right Back
+        frontRight = hardwareMap.dcMotor.get("frontRight"); //Right Front
+//        evangelino = hardwareMap.dcMotor.get("lift");
+//        collector = hardwareMap.dcMotor.get("collector");
+//        teamMarker = hardwareMap.servo.get("teamMarker");
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+//        liftrawrh = hardwareMap.servo.get("liftrawrh");
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
 
         //Variables//
-        private double xValue, yValue, lValue, rValue, leftPower, rightPower;
-        public int calibToggle;
-        public int target;
-        private double speed;
-        private Servo liftrawrh;
+        calibToggle = 0;
+        driveMode = 0;
+//        target = 0;
+//        speed = 0;
+    }
 
-        //HardwareCatBot robot;
+    public void loop() {
 
+        //robot = new Hardware_Pushbot();
+        if (gamepad1.a && (runtime.seconds() > calibToggle)) {
+            calibToggle = runtime.seconds() + 1;
+            ++driveMode;
+        }
 
-        public void init() {
-            //Motors//
-            backLeft = hardwareMap.dcMotor.get("backLeft"); //Left Back
-            frontLeft = hardwareMap.dcMotor.get("frontLeft"); //Left Front
-            backRight = hardwareMap.dcMotor.get("backRight"); //Right Back
-            frontRight = hardwareMap.dcMotor.get("frontRight"); //Right Front
-//            evangelino = hardwareMap.dcMotor.get("lift");
-//            //collector = hardwareMap.dcMotor.get("collector");
-//            teamMarker = hardwareMap.servo.get("teamMarker");
-            backLeft.setDirection(DcMotor.Direction.REVERSE);
-//            liftrawrh = hardwareMap.servo.get("liftrawrh");
-            frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        coarseDiff = .6; //Different offsets
+        fineDiff = .3;
+        stickDiff = .9;
 
-            //Variables//
-            calibToggle = 0;
-            int target = 0;
-            double speed = 0;}
+        yValue = (gamepad1.left_stick_y);
+        xValue = (gamepad1.right_stick_x * stickDiff); //Multiplied by .92 to eliminate over-turning
 
-        public void loop() {
+        if (driveMode % 2 == 0) { //Coarse Drive Mode
+            leftPower = Math.pow(yValue - xValue, 3) * coarseDiff;
+            rightPower = Math.pow(yValue + xValue, 3) * coarseDiff;
 
-            //robot = new Hardware_Pushbot();
-            if (gamepad1.a) {
-                calibToggle = 1;
-            } else if (gamepad1.b) {
-                calibToggle = 0;
-            }
+            telemetry.addData("Mode: ", "COARSE");
+            telemetry.addData("Stick: ", "Y = " + round(yValue, 3) + ", X = " + round(xValue / stickDiff, 3));
+            telemetry.addData("Power: ", "L = " + round(leftPower / coarseDiff, 3) + ", R = " + round(rightPower / coarseDiff, 3));
+            telemetry.update();
+        } else { //Fine Drive Mode
+            leftPower = Math.pow(yValue - xValue, 3) * fineDiff;
+            rightPower = Math.pow(yValue + xValue, 3) * fineDiff;
 
-            if (calibToggle == 0) { //A
-                yValue = (gamepad1.left_stick_y);
-                xValue = (gamepad1.left_stick_x * .75);
+            telemetry.addData("Mode: ", "FINE");
+            telemetry.addData("Stick: ", "Y = " + round(yValue, 3) + ", X = " + round(xValue / stickDiff, 3));
+            telemetry.addData("Power: ", "L = " + round(leftPower / fineDiff, 3) + ", R = " + round(rightPower / fineDiff, 3));
+            telemetry.update();
+        }
 
-                leftPower = Math.pow(yValue - xValue, 3);
-                rightPower = Math.pow(yValue + xValue, 3);
+        backLeft.setPower(leftPower);
+        backRight.setPower(rightPower);
+        frontLeft.setPower(leftPower);
+        frontRight.setPower(rightPower);
+    }
 
-                backLeft.setPower(Range.clip(leftPower, -0.6, 0.6));
-                backRight.setPower(Range.clip(rightPower, -0.6, 0.6));
-                frontLeft.setPower(Range.clip(leftPower, -0.6, 0.6));
-                frontRight.setPower(Range.clip(rightPower, -0.6, 0.6));
+    public static double round(double value, int places) { //Allows telemetry to display nicely
+        if (places < 0) throw new IllegalArgumentException();
 
-                telemetry.addData("Mode: ", "Coarse");
-                telemetry.addData("Stick: ", "Y = " + yValue + ", X = " + xValue / .75);
-                telemetry.addData("Power: ", "L = " + leftPower + ", R = " + rightPower);
-                telemetry.update();
-
-            } else if (calibToggle == 1) { //B
-                yValue = (gamepad1.left_stick_y);
-                xValue = (gamepad1.left_stick_x * .75);
-
-                leftPower = Math.pow(yValue - xValue, 3) * .75;
-                rightPower = Math.pow(yValue + xValue, 3) * .75;
-
-                backLeft.setPower(Range.clip(leftPower, -0.3, 0.3));
-                backRight.setPower(Range.clip(rightPower, -0.3, 0.3));
-                frontLeft.setPower(Range.clip(leftPower, -0.3, 0.3));
-                frontRight.setPower(Range.clip(rightPower, -0.3, 0.3));
-
-                telemetry.addData("Mode: ", "Fine");
-                telemetry.addData("Stick: ", "Y = " + yValue + ", X = " + xValue / .75);
-                telemetry.addData("Power: ", "L = " + leftPower / .75 + ", R = " + rightPower / .75);
-                telemetry.update();
-//                lValue = Math.pow((gamepad1.left_stick_y * .6), 3);
-//                rValue = Math.pow((gamepad1.right_stick_y * .6), 3);
-//
-//                backLeft.setPower(lValue);    //Tank Drive
-//                frontLeft.setPower(lValue);
-//                backRight.setPower(rValue);
-//                frontRight.setPower(rValue);
-//
-//                telemetry.addData("Mode: ", "Fine");
-//                telemetry.addData("Power: ", "L = " + leftPower + ", R = " + rightPower);
-//                telemetry.update();
-            }
-
-        }}
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+}
 
