d00f116a76496d41b73e8698bf788a9d5baaad30
Nathan Stewart <natedawg1121@gmail.com>
2018-10-14 20:16:53 -0400

    edited drivecode for 4Motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp4motors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp4motors.java
@@ -19,9 +19,10 @@ public class TeleOp4motors extends OpMode {
     private DcMotor backRight;
 
     //Variables//
-    private double xValue, yValue, leftPower, rightPower, coarseDiff, fineDiff, stickDiff;
-    public double calibToggle;
-    public int driveMode;
+    private double leftStick, rightStick;
+    private double leftPower, rightPower;
+    private double calibToggle, coarseDiff, fineDiff, stickDiff;
+    private int driveSpeed, driveMode;
 
     //Objects//
     public ElapsedTime runtime = new ElapsedTime();
@@ -39,53 +40,112 @@ public class TeleOp4motors extends OpMode {
 
         //Variables//
         calibToggle = 0;
+        driveSpeed = 0;
         driveMode = 0;
     }
 
 
     public void loop() {
-        //Toggle Button
+        // TOGGLE BUETONS //
         if (gamepad1.a && (runtime.seconds() > calibToggle)) {
             calibToggle = runtime.seconds() + 1;
-            ++driveMode;
+            ++driveSpeed;
+        }
+        if (gamepad1.x && (runtime.seconds() > calibToggle)) {
+            driveMode = 0;
+        }
+        if (gamepad1.y && (runtime.seconds() > calibToggle)) {
+            driveMode = 1;
+        }
+        if (gamepad1.b && (runtime.seconds() > calibToggle)) {
+            driveMode = 2;
         }
 
-        //Different Offsets
-        coarseDiff = .75;
-        fineDiff = .45;
-        stickDiff = .9;
+        if (driveMode == 0) {
+            //////////////////////////////////// ARCADE DRIVE //////////////////////////////////////
 
-        yValue = (gamepad1.left_stick_y);
-        xValue = (-gamepad1.right_stick_x * stickDiff);
+            //Speed Offsets
+            coarseDiff = .75;
+            fineDiff = .45;
+            stickDiff = 1;
 
-        //Left Side
-        if (Math.abs(xValue) > 0.5) {
-            if (Math.abs(yValue) > 0.5) {
-                leftPower = yValue / 2 + xValue / 2;
+            leftStick = (gamepad1.left_stick_y);
+            rightStick = (-gamepad1.right_stick_x * stickDiff);
+
+            //Left Side
+            if (Math.abs(rightStick) > 0.45) {
+                if (Math.abs(leftStick) > 0.45) {
+                    leftPower = leftStick / 2 + rightStick / 2;
+                } else {
+                    leftPower = leftStick + rightStick;
+                }
             } else {
-                leftPower = yValue + xValue;
+                leftPower = leftStick;
             }
-        } else {
-            leftPower = yValue;
-        }
 
-        //Right Side
-        if (Math.abs(xValue) > 0.5) {
-            if (Math.abs(yValue) > 0.5) {
-                rightPower = yValue / 2 - xValue / 2;
+            //Right Side
+            if (Math.abs(rightStick) > 0.45) {
+                if (Math.abs(leftStick) > 0.45) {
+                    rightPower = leftStick / 2 - rightStick / 2;
+                } else {
+                    rightPower = leftStick - rightStick;
+                }
             } else {
-                rightPower = yValue - xValue;
+                rightPower = leftStick;
             }
-        } else {
-            rightPower = yValue;
+        } else if (driveMode == 1) {
+            ///////////////////////////////////// TANK DRIVE ///////////////////////////////////////
+
+            //Speed Offsets
+            coarseDiff = .75;
+            fineDiff = .45;
+
+            leftPower = (gamepad1.left_stick_y * coarseDiff);
+            rightPower = (gamepad1.right_stick_y * coarseDiff);
+        } else if (driveMode == 2) {
+            ////////////////////////////////// ACKERMAN DRIVE //////////////////////////////////////
+
+            //Speed Offsets
+            coarseDiff = .75;
+            fineDiff = .45;
+
+            leftStick = (gamepad1.left_stick_y);
+            rightStick = 1;
+
+            if (gamepad1.right_trigger > 0.15) {
+                if (Math.abs(rightStick) > 0.45) {
+                    leftPower = leftStick / 2 + gamepad1.right_trigger;
+                } else {
+                    leftPower = leftStick;
+                }
+                if (Math.abs(rightStick) > 0.45) {
+                    rightPower = leftStick / 2 - gamepad1.right_trigger;
+                } else {
+                    rightPower = leftStick;
+                }
+            } else if (gamepad1.left_trigger > 0.15) {
+                if (Math.abs(rightStick) > 0.45) {
+                    leftPower = leftStick / 2 + gamepad1.left_trigger;
+                } else {
+                    leftPower = leftStick;
+                }
+                if (Math.abs(rightStick) > 0.45) {
+                    rightPower = leftStick / 2 - gamepad1.left_trigger;
+                } else {
+                    rightPower = leftStick;
+                }
+            } else {
+                leftPower = 0;
+                rightPower = 0;
+            }
         }
 
-        if (driveMode % 2 == 0) { //Coarse Drive Mode
+        if (driveSpeed % 2 == 0) { //Coarse Drive Mode
             leftPower = Math.pow(leftPower, 3) * coarseDiff;
             rightPower = Math.pow(rightPower, 3) * coarseDiff;
 
             telemetry.addData("Mode: ", "COARSE");
-            telemetry.addData("Stick: ", "Y = " + round(yValue, 3) + ", X = " + round(xValue, 3));
+            telemetry.addData("Stick: ", "Y = " + round(leftStick, 3) + ", X = " + round(rightStick, 3));
             telemetry.addData("Power: ", "L = " + round(leftPower / coarseDiff, 3) + ", R = " + round(rightPower / coarseDiff, 3));
             telemetry.update();
         } else { //Fine Drive Mode
@@ -93,7 +153,7 @@ public class TeleOp4motors extends OpMode {
             rightPower = Math.pow(rightPower, 3) * fineDiff;
 
             telemetry.addData("Mode: ", "FINE");
-            telemetry.addData("Stick: ", "Y = " + round(yValue, 3) + ", X = " + round(xValue, 3));
+            telemetry.addData("Stick: ", "Y = " + round(leftStick, 3) + ", X = " + round(rightStick, 3));
             telemetry.addData("Power: ", "L = " + round(leftPower / fineDiff, 3) + ", R = " + round(rightPower / fineDiff, 3));
             telemetry.update();
         }
