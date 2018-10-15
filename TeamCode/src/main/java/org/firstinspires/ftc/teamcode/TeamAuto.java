23911cc8a9fdcd7f4aecbc5fc2a3b86ea272fdc3
joannareese <jdreese2016@gmail.com>
2018-10-15 19:34:22 -0400

    Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamAuto.java
@@ -2,44 +2,64 @@ package org.firstinspires.ftc.teamcode;
 //EXIST
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @Autonomous(name="Team Autonomous")
 public class TeamAuto extends LinearOpMode {
-
+    private Servo liftrawrh;
+    private Servo flickyWrist;
     @Override public void runOpMode() {
         //initialization
-        Navigation nav = new Navigation(this,telemetry,true,false, true);
+        //liftrawrh = hardwareMap.servo.get("liftrawrh");
+        //flickyWrist = hardwareMap.servo.get("flicky");
+
+        //liftrawrh.setPosition(1.0);
+        //flickyWrist.setPosition(0.5);
+        Navigation nav = new Navigation(this,telemetry, true, false, true);
+
+        waitForStart();
 
         //try determine vision stuff
        // nav.updatePos();
         //nav.updateTeam();
-        while (!isStarted()) {
+
             nav.updateCubePos();
 
-        }
-        while (!isStopRequested()){
+
             nav.getCubePos();
-        }
 
         //try determine vision stuff
         //nav.updatePos();
         //nav.updateTeam();
         nav.updateCubePos();
-
+        nav.driveEncoderReset();
 
         //landing motor commands
-
+//        liftrawrh.setPosition(0.3);
+        nav.rotateTo(100f, 200f, 40f);
+        sleep(100);
+//        //Start lift down motors(2) go down, need method (note to self), reverse one motor
+        nav.rotateTo(0f, 200f, 40f);
+        nav.goDistance(200f,50f);
+        nav.drivePower(0,0);
         //try determine vision stuff
         //nav.updatePos();
-        nav.updateTeam();
-        nav.updateCubePos();
 
-        //goto cube
+//        nav.updateTeam();
+//        nav.updateCubePos();
+
+        //go to cube
+       // nav.setLift(100);
         //collect/nudge
+
         //goto depot
+
         //deploy team marker thing
+
         //go to appropriate crater
+
         //begin collection
+
         //parking
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name = "Teleop", group = "Teleop")
+@TeleOp(name = "NotTeleop", group = "NotTeleop")
 
 public class Teleop extends OpMode {
 
