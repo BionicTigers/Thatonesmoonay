020883663e157dc5cfbf20a174267d681b13a5ea
ADMIN <qcjames53@gmail.com>
2018-10-21 19:43:52 -0400

    Dogeforia fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MultipartDebug.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MultipartDebug.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name="Debugger TeleOp", group="BTBT")
+@TeleOp(name="Multipart Debug", group="BTBT")
 public class MultipartDebug extends OpMode   {
 
     private DcMotor frontLeft;
