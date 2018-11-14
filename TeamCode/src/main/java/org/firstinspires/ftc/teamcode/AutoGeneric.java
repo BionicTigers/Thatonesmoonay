d046d58b69774e906ca9907a747078699ee5a9bd
codingchris779 <33784082+codingchris779@users.noreply.github.com>
2018-11-14 18:47:17 -0500

    Chris has added cool init sampling to generic auto :) TODO @JoJo pls text the new init sampling make sure that the cam is pointed at the minerals and only the minerals in init
    (cherry picked from commit c9d7020eacc206c9c5b45f8c8a0448345998c3ba)
    
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoGeneric.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoGeneric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoGeneric.java
@@ -1,42 +1,36 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.internal.webserver.RobotControllerWebHandlers;
 
-public class AutoGeneric{
+public class AutoGeneric extends LinearOpMode{
 
     public static enum StartPos {DEPOT, CRATER};
     private StartPos startZone;
-    private OpMode opMode;
-    private Telemetry telemetry;
-    private Navigation nav;
+    Navigation.CubePosition initvalue;
 
-    public AutoGeneric(StartPos startZone, com.qualcomm.robotcore.eventloop.opmode.OpMode opMode, org.firstinspires.ftc.robotcore.external.Telemetry telemetry) {
+    public AutoGeneric(StartPos startZone) {
         this.startZone = startZone;
-        this.opMode = opMode;
-        this.telemetry = telemetry;
-        nav = new Navigation(opMode, telemetry,true);
     }
 
     public void runOpMode() {
-
+        Navigation nav = new Navigation(this, telemetry,true);
 
         //extendy - in
         //lift - down
         //collector height
 
-        nav.setLiftLock(Navigation.LiftLock.LOCK);
+        //nav.setLiftLock(Navigation.LiftLock.LOCK);
         nav.setCollectorHeight(Navigation.CollectorHeight.DUMP);
+      while (!opModeIsActive()){
+        nav.updateCubePos();
+        initvalue = nav.getCubePos();
 
         //Sampling on intit
         nav.updateCubePos();
-        nav.setLiftLock(Navigation.LiftLock.LOCK);
 
-
+        waitForStart();
 
         //detaching from hook w evan method
         //nav.setCollectorExtension(Navigation.CollectorExtension.DUMP);
@@ -44,50 +38,49 @@ public class AutoGeneric{
         //nav.setLiftHeight(Navigation.LiftHeight.HOOK);
 
         //detaching from hook w nick method
-        nav.setLiftLock(Navigation.LiftLock.UNLOCK);
         nav.setCollectorHeight(Navigation.CollectorHeight.COLLECT);
         nav.setLiftHeight(Navigation.LiftHeight.HOOK);
-        nav.pointTurnRelative(45f,45f,2f);
+        nav.pointTurnRelative(45f,45f,5f);
         nav.setLiftHeight(Navigation.LiftHeight.LOWER);
-        nav.pointTurnRelative(-45f,45f,2f);
+        nav.pointTurnRelative(-45f,45f,5f);
 
 
 
 
 
-        nav.goDistance(20f,30f);
+        nav.goDistance(20f,5f);
         nav.setCollectionSweeper(Navigation.CollectorSweeper.INTAKE);
 
-        switch(nav.getCubePos()) {
+        switch(initvalue) {
             case LEFT:
-                nav.pointTurnRelative(45f,45f,2f);
+                nav.pointTurnRelative(45f,45f,5f);
                 nav.setCollectorExtension(Navigation.CollectorExtension.OUT);
                 nav.setLiftHeight(Navigation.LiftHeight.LOWER);
                 nav.setCollectorExtension(Navigation.CollectorExtension.DUMP);
-                nav.pointTurnRelative(30f,30f,2f);
+                nav.pointTurnRelative(30f,30f,5f);
                 break;
             case MIDDLE:
                 nav.setCollectorExtension(Navigation.CollectorExtension.OUT);
                 nav.setLiftHeight(Navigation.LiftHeight.LOWER);
                 nav.setCollectorExtension(Navigation.CollectorExtension.DUMP);
-                nav.pointTurnRelative(75f,30f,2f);
+                nav.pointTurnRelative(75f,30f,5f);
                 break;
             default:
-                nav.pointTurnRelative(-45f,45f,2f);
+                nav.pointTurnRelative(-45f,45f,5f);
                 nav.setCollectorExtension(Navigation.CollectorExtension.OUT);
                 nav.setLiftHeight(Navigation.LiftHeight.LOWER);
                 nav.setCollectorExtension(Navigation.CollectorExtension.DUMP);
-                nav.pointTurnRelative(120f,30f,2f);
+                nav.pointTurnRelative(120f,30f,5f);
                 break;
         }
 
         nav.goDistance(45f,20f);
 
         if(startZone == StartPos.CRATER) {
-            nav.pointTurnRelative(60f,30f,2f);
+            nav.pointTurnRelative(60f,30f,5f);
         }
         else {
-            nav.pointTurnRelative(-120f,30f,2f);
+            nav.pointTurnRelative(-120f,30f,5f);
         }
 
 
@@ -96,4 +89,5 @@ public class AutoGeneric{
         nav.setCollectorExtension(Navigation.CollectorExtension.OUT);
 
     }
+ }
 }
