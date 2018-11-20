edb5e0130b26b9ae18a7a04e1004176cd20d4f6f
joannareese <jdreese2016@gmail.com>
2018-11-20 11:17:43 -0500

    This is double sampling and stuff from scrimmage rawrhhh

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telem.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "telem")
+public class Telem extends OpMode {
+    private Navigation nav;
+    @Override
+    public void init() {
+        nav = new Navigation(this, telemetry,true);
+
+    }
+
+    @Override
+    public void loop() {
+        nav.telemetryMethod();
+        nav.updateCubePos();
+
+    }
+}
