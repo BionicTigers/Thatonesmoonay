29dc8f9698e43953a6f7696884e4e6eafcdfa80f
ADMIN <qcjames53@gmail.com>
2018-11-20 20:12:53 -0500

    Another commit. Have fun looking for any specific changes.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMotorPositions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMotorPositions.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@Disabled
 @Autonomous(name="Test Motor Positions", group="Test")
 public class TestMotorPositions extends LinearOpMode {
 
